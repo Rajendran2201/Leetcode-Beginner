7cf64de6ba2359e534525146946788fdcac3e935
Raj <137254223+Rajendran2201@users.noreply.github.com>
2024-09-16 01:32:34 +0530

    Time: 0 ms (100%), Space: 40.2 MB (56.7%) - LeetHub

--- 1761-count-sorted-vowel-strings/1761-count-sorted-vowel-strings.java
+++ 1761-count-sorted-vowel-strings/1761-count-sorted-vowel-strings.java
@@ -8,28 +8,15 @@ class Solution {
         if (r > n || r < 0) {
             return 0;
         }
-
          if (r == 0 || r == n) {
             return 1; // C(n, 0) or C(n, n) is 1
         }
-
-         long result = 1;
-
+        long result = 1;
         // Calculate nCr using the multiplicative formula to avoid overflow
         for (int i = 0; i < r; i++) {
             result *= (n - i);
             result /= (i + 1);
         }
-
         return result;
-    }
-
-    // Function to calculate factorial
-    public long factorial(int n) {
-        long fact = 1;
-        for (int i = 2; i <= n; i++) {
-            fact *= i;
-        }
-        return fact;
     }
 }
