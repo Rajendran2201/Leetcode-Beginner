55b128744d02d845708ca6a4387ac255a09957cb
Raj <137254223+Rajendran2201@users.noreply.github.com>
2024-09-16 20:50:58 +0530

    Time: 1 ms (98.61%), Space: 41.3 MB (70.34%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -2,29 +2,13 @@ class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
         for(char c : s.toCharArray()){
-            if (c == '(' || c == '[' || c == '{'){
-                stack.push(c);
-            }else{
-                if(stack.isEmpty()){
-                    
-                    return false;
-                }
-                if(c == ')'){
-                    if(stack.pop() != '('){
-                        return false;
-                    }
-                }
-                if(c == ']'){
-                    if(stack.pop() != '['){
-                        return false;
-                    }
-                }
-                if(c == '}'){
-                    if(stack.pop() != '{'){
-                        return false;
-                    }
-                }
-            }
+          if(c == '('){stack.push(')');}
+          else if(c == '['){stack.push(']');}
+          else if(c == '{'){stack.push('}');}
+          else if(stack.isEmpty() || c != stack.pop()){
+            return false;
+          }
+          
         }
         return stack.isEmpty();
     }
