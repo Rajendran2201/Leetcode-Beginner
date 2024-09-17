8eed726c8ceac0f6ab795c7ae975890441e72552
Raj <137254223+Rajendran2201@users.noreply.github.com>
2024-09-17 21:29:04 +0530

    Time: 25 ms (5.61%), Space: 45.5 MB (7.71%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,27 +1,28 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        
         int[] ans = new int[nums1.length];
         for(int i=0; i<nums1.length; i++){
-            ans[i] = findNextGreaterElement(nums1[i], nums2);
+            ans[i] = findNGE(nums1[i], nums2);
         }
         return ans;
     }
-    public int findNextGreaterElement(int x, int[] nums2){
-        int index = findIndex(x, nums2);
-        if(index == -1){
-            return index;
-        }
-        for(int i = index; i<nums2.length; i++){
-            if(nums2[i] > x){
-                return nums2[i];
-            }
-        }
-        return -1;
-    }
-    public int findIndex(int x, int[] nums){
-        for(int i=0; i<nums.length; i++){
+    public int findNGE(int x, int[] nums){
+        Stack<Integer> stack = new Stack<>();
+        for(int i=nums.length-1; i>=0; i--){
             if(nums[i] == x){
-                return i;
+                while(true){
+                    if(stack.isEmpty()){
+                        return -1;
+                    }
+                    int max = stack.pop();
+                    if(max > x){
+                        return max;
+                    }
+                }
+                
+            }else{
+                stack.push(nums[i]);
             }
         }
         return -1;
