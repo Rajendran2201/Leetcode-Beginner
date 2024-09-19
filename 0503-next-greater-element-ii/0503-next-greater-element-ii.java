bd5132f78a58a5a52080f6a86438fd048feb799a
Raj <137254223+Rajendran2201@users.noreply.github.com>
2024-09-19 21:23:05 +0530

    Time: 90 ms (8.29%), Space: 46.3 MB (36%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -1,22 +1,17 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
-       int[] res = new int[nums.length];
-       for(int i=0; i<nums.length; i++){
-            res[i] = findNGE(nums[i], i, nums);
-       }
-       return res;
-    }
-    public int findNGE(int x, int index, int[] nums){
-       for(int i=index; i<nums.length; i++){
-        if(nums[i] > x){
-            return nums[i];
+        int[] res = new int[nums.length];
+        int n = nums.length;
+        Arrays.fill(res, -1);
+       for(int i=0; i<n; i++){
+        for(int j=1; j<n; j++){
+            int index = (i+j) % n;
+            if(nums[index] > nums[i]){
+                res[i] = nums[index];
+                break;
+            }
         }
        }
-       for(int i=0; i<index; i++){
-         if(nums[i] > x){
-            return nums[i];
-        }
-       }
-       return -1;
+        return res;
     }
 }
