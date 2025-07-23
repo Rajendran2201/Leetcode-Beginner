fd5466ceab58342b0bcc97568f09a15623978281
Raj <137254223+Rajendran2201@users.noreply.github.com>
2025-07-23 07:11:08 +0530

    Time: 0 ms (100%), Space: 48.1 MB (17.56%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -15,26 +15,38 @@
  */
 class Solution {
     public int countNodes(TreeNode root) {
-        Queue<TreeNode> queue = new LinkedList<>();
+        // Here's the optimised version 
+        // The ideas here is quite simple. All we have to know is to understand a property of a full BT.
+        // The maximum number of nodes in a full BT is 2^h - 1 where h is the height of the BT
+        // A complete BT is a collection of FULL Binary Subtrees. 
+
+        // So, here's the algo:
+            // FInd the left height and right height of the root node
+            // If both are equal, the complete BT is a full binary tree, apply the formula
+            // If not apply the formula separtely to the left subtree and right subtree
+            // add the resultants together. Also add 1 extra for the root node
+        if(root == null) return 0;
+        int leftLength = findLeftCount(root);
+        int rightLength = findRightCount(root);
+
+        if(leftLength == rightLength)
+            return (int)Math.pow(2, leftLength) - 1;
+        
+        return 1 + countNodes(root.left) + countNodes(root.right);
+    }
+    public int findLeftCount(TreeNode node){
         int count = 0;
-        if(root != null){
-            queue.add(root);
+        while(node != null){
+            node = node.left;
             count++;
         }
-
-        while(!queue.isEmpty()){
-            int size = queue.size();
-            for(int i=0; i<size; i++){
-                TreeNode node = queue.poll();
-                if(node.left != null){
-                    queue.add(node.left);
-                    count++;
-                }
-                if(node.right != null){
-                    queue.add(node.right);
-                    count++;
-                }
-            }
+        return count;
+    }
+    public int findRightCount(TreeNode node){
+        int count = 0;
+        while(node != null){
+            node = node.right;
+            count++;
         }
         return count;
     }
